2798c2ac4aea7a4336b8a66e12e8222a936eed7b
AkshajIGF <akshajsr000@gmail.com>
2025-07-01 13:49:05 +0200

    ip

--- app/components/video-call/VideoCallDemo.tsx
+++ app/components/video-call/VideoCallDemo.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { VideoCall } from './VideoCall';
 import { CallEndedScreen } from './CallEndedScreen';
 import { Participant } from './types';
@@ -6,6 +6,16 @@ import { Participant } from './types';
 export const VideoCallDemo: React.FC = () => {
     const [callState, setCallState] = useState<'active' | 'ended'>('active');
     const [callDuration, setCallDuration] = useState(0);
+    const [returnIp, setReturnIp] = useState<string>('');
+
+    useEffect(() => {
+        // Get the IP from URL parameters when component mounts
+        const params = new URLSearchParams(window.location.search);
+        const ip = params.get('ip');
+        if (ip) {
+            setReturnIp(ip);
+        }
+    }, []);
 
     const participant: Participant = {
         id: 1,
@@ -26,7 +36,9 @@ export const VideoCallDemo: React.FC = () => {
     };
 
     const closeTab = () => {
-        window.location.href = 'http://10.159.246.31';
+        if (returnIp) {
+            window.location.href = `http://${returnIp}`;
+        }
     };
 
     switch (callState) {
